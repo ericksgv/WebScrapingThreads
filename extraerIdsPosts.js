114db2dd6a3bae353b2d1820c94a5554d5a0ac11
Erick Garavito <sangaravi22@gmail.com>
2024-04-24 05:32:48 -0500

    Generar JSON requerido

--- extraerIdsPosts.js
+++ extraerIdsPosts.js
@@ -1,5 +1,15 @@
 const fs = require('fs');
 
+// Función para convertir un timestamp en microsegundos a una fecha legible
+function convertirTimestampALegible(timestamp) {
+    const milisegundos = timestamp / 1000; // Convertir a milisegundos
+    const fecha = new Date(milisegundos);
+    const dia = fecha.getDate();
+    const mes = fecha.getMonth() + 1; // Se suma 1 porque los meses se indexan desde 0
+    const año = fecha.getFullYear();
+    return `${dia}-${mes}-${año}`;
+}
+
 // Crear una lista vacía para almacenar todos los IDs de los posts
 let all_ids = [];
 
@@ -10,6 +20,7 @@ try {
     // Parsear el contenido JSON
     const datos = JSON.parse(datosJSON);
 
+    const publicaciones = [];
     // Iterar sobre cada objeto en el array
     datos.forEach(item => {
         // Recorrer todos los elementos en 'threads' si existe
@@ -18,6 +29,21 @@ try {
             (thread.thread_items || []).forEach(thread_item => {
                 // Intentar acceder al ID del post
                 try {
+                    const descripcion = thread_item.post.caption.text;
+                    const comentarios = [];
+                    const fecha = convertirTimestampALegible(thread_item.post.device_timestamp);
+                    const fuente = "Threads";
+
+                    // Crear un objeto para la publicación actual
+                    const publicacion = {
+                        descripcion: descripcion,
+                        comentarios: comentarios,
+                        fecha: fecha,
+                        fuente: fuente
+                    };
+
+                    // Agregar la publicación al array de publicaciones
+                    publicaciones.push(publicacion);
                     const post_id = thread_item.post.id.split('_')[0]; // Obtener solo la parte antes del guion bajo
                     all_ids.push(post_id);
                 } catch (error) {
@@ -27,6 +53,21 @@ try {
             });
         });
     });
+
+
+    // Crear el objeto JSON final con el array de publicaciones
+    const jsonPublicaciones = {
+        publicaciones: publicaciones
+    };
+
+    // Convertir el objeto JSON a una cadena
+    const jsonStr = JSON.stringify(jsonPublicaciones, null, 2);
+
+    // Mostrar el objeto JSON
+    console.log(jsonStr);
+    
+    // Escribir el objeto JSON en un archivo
+    fs.writeFileSync('publicaciones.json', jsonStr, { encoding: 'utf-8', flag: 'w' });
 
     // Escribir todos los IDs de los posts en un archivo txt
     fs.writeFileSync('idsPosts.txt', all_ids.join('\n'), { encoding: 'utf-8', flag: 'w' });
